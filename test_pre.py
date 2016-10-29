309d4122deeb90a2e84eb334de87e28b9da8a890
jknees <cortezcardinal@gmail.com>
2016-10-28 19:32:19 -0700

    Cleaned up code

--- test_pre.py
+++ test_pre.py
@@ -7,10 +7,6 @@ def test_location():
 	raw = open("poi.txt")
 	places = pre.process(raw)
 	assert places[0]['name'] == 'Student Tennis Center'
-	print("lat = " + str(places[0]['lat']))
-	print("lon = " + str(places[0]['lon']))
-	print(type(places[0]['lat']))
-	print(type(places[0]['lon']))
 	assert places[0]['lat'] == '44.04157'
 	assert places[0]['lon'] == '-123.0732'
 
