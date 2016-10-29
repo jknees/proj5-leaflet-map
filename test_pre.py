6f4cc2ab572f96bee9737be5040cd20cceb8c31d
jknees <cortezcardinal@gmail.com>
2016-10-28 19:22:25 -0700

    Checking bugs in test

--- test_pre.py
+++ test_pre.py
@@ -8,7 +8,7 @@ def test_location():
 	places = pre.process(raw)
 	assert places[0]['name'] == 'Student Tennis Center'
 	assert places[0]['lat'] == '44.04157'
-	assert places[0]['lon'] == -123.0732
+	assert places[0]['lon'] == '-123.0732'
 
 	assert places[1]['name'] == 'Student Tennis Courts'
 	assert places[1]['lat'] == '44.04044'
