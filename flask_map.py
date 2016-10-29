628d74484861cb17ce1fb5b99ad5943b61d9d537
jknees <cortezcardinal@gmail.com>
2016-10-28 17:45:45 -0700

    Added additional Flask boiler plate

--- flask_map.py
+++ flask_map.py
@@ -1,4 +1,12 @@
 import flask
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask import jsonify # For AJAX transactions
+
+import json
+import logging
+
 
 app = app.Flask(__name__)
 import CONFIG
@@ -9,3 +17,17 @@ def index():
 	raw = open('places.txt')
 	flask.session['Places'] = pre.process(raw)
 	return flask.rendertemplate('index.html')
+
+
+if __name__ == "__main__":
+    # Standalone. 
+    app.debug = True
+    app.logger.setLevel(logging.DEBUG)
+    print("Opening for global access on port {}".format(CONFIG.PORT))
+    app.run(port=CONFIG.PORT, host="0.0.0.0")
+else:
+    # Running from cgi-bin or from gunicorn WSGI server, 
+    # which makes the call to app.run.  Gunicorn may invoke more than
+    # one instance for concurrent service.
+    #FIXME:  Debug cgi interface 
+    app.debug=False
