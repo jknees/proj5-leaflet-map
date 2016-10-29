37d2f66f770bf394a00bd72d76920cad4093a367
jknees <cortezcardinal@gmail.com>
2016-10-28 19:24:27 -0700

    Added debugging to test

--- test_pre.py
+++ test_pre.py
@@ -9,6 +9,8 @@ def test_location():
 	assert places[0]['name'] == 'Student Tennis Center'
 	assert places[0]['lat'] == '44.04157'
 	assert places[0]['lon'] == '-123.0732'
+	print("lat = " + str(places[0]['lat']))
+	print("lon = " + str(places[0]['lon']))
 
 	assert places[1]['name'] == 'Student Tennis Courts'
 	assert places[1]['lat'] == '44.04044'
