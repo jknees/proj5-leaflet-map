e536d45e804068ea2bdf1c85bee61e941436fc09
jknees <cortezcardinal@gmail.com>
2016-10-28 19:21:52 -0700

    Checking bugs in test

--- test_pre.py
+++ test_pre.py
@@ -7,7 +7,7 @@ def test_location():
 	raw = open("poi.txt")
 	places = pre.process(raw)
 	assert places[0]['name'] == 'Student Tennis Center'
-	assert places[0]['lat'] == 44.04157
+	assert places[0]['lat'] == '44.04157'
 	assert places[0]['lon'] == -123.0732
 
 	assert places[1]['name'] == 'Student Tennis Courts'
